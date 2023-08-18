af25e9197c9b8344b02c192e4ca34d7f46e30c11
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2023-08-18 15:22:02 -0300

    adding image

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Avatar = () => {
+  return (
+      <div className='hidden xl:flex xl:max-w-none'>
+          <img src="/avatar.png" width={737} height={678}
+          className='translate-z-0 w-full h-full'/>
+    </div>
+  )
+}
+
+export default Avatar
